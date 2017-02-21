9aa59a6b8d4d13cccf43d77401a9aa20ba7b9b39
zileyue <zile.pang@gmail.com>
2017-02-21 22:16:38 +0800

    add bigdigits_ans.py

---
+++ bigdigits_ans.py
@@ -0,0 +1,60 @@
+#! /usr/bin/python3
+#coding=utf-8
+
+
+import sys
+
+
+Zero = ["  ***  ",
+        " *   * ",
+        "*     *",
+        "*     *",
+        "*     *",
+        " *   * ",
+        "  ***  "]
+One = [" * ", "** ", " * ", " * ", " * ", " * ", "***"]
+Two = [" *** ", "*   *", "*  * ", "  *  ", " *   ", "*    ", "*****"]
+Three = [" *** ", "*   *", "    *", "  ** ", "    *", "*   *", " *** "]
+Four = ["   *  ", "  **  ", " * *  ", "*  *  ", "******", "   *  ",
+        "   *  "]
+Five = ["*****", "*    ", "*    ", " *** ", "    *", "*   *", " *** "]
+Six = [" *** ", "*    ", "*    ", "**** ", "*   *", "*   *", " *** "]
+Seven = ["*****", "    *", "   * ", "  *  ", " *   ", "*    ", "*    "]
+Eight = [" *** ", "*   *", "*   *", " *** ", "*   *", "*   *", " *** "]
+Nine = [" ****", "*   *", "*   *", " ****", "    *", "    *", "    *"]
+
+BigDigits = [Zero,One,Two,Three,Four,Five,Six,Seven,Eight,Nine]
+
+if len(sys.argv) < 2:
+    print('Usage: ./bigdigits.py numbers_you_want_to_show_in_big_digit')
+    sys.exit()
+
+input_digits = sys.argv[1]
+print('the input digit is: ',input_digits)
+input_digits_count = len(input_digits)
+
+print()
+row = 0
+while row < 7:
+
+    line = ''
+    count = 0
+    while count < input_digits_count:
+        digit = input_digits[count]
+        try:
+            number = int(digit)
+            s = list(BigDigits[number][row])
+            i = 0
+            while i < len(s):
+                if s[i] == '*':
+                    s[i] = digit
+                line = line + s[i] + ' '
+                i += 1
+
+        except ValueError as err:
+           print('%s is not a digit!',digit)
+        count += 1
+
+    print(line)
+    row += 1
+
